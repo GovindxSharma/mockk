9497d96353606e745c1bf04e2338b593b3710d36
GovindxSharma <govindsharma2839@gmail.com>
2023-12-09 11:36:48 +0530

    Layout-partial & router-controller

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express= require('express')
-const port=8000;
-
-
 const app=express();
+const port=8080;
+const expressLayouts=require('express-ejs-layouts')
+
+app.use(expressLayouts)
+
+app.use('/',require('./routes'))
+
+app.set('view engine','ejs')
+
+app.set('views','./views')
+
+
 
 app.listen(port,()=>console.log(`Server is running at port : ${port} `))
